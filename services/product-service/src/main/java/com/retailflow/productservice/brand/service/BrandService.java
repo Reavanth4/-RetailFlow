4843e4fd48063e3d7182a587bf92091cecc6bbc4
SRIDHAR  S <156069624+Sridhar0112@users.noreply.github.com>
2026-07-28 00:08:22 +0530

    Revert "Brand enhancement (#3)"
    This reverts commit a391990582a57349cec1667a42b45ae5ab91f542.

--- services/product-service/src/main/java/com/retailflow/productservice/brand/service/BrandService.java
+++ services/product-service/src/main/java/com/retailflow/productservice/brand/service/BrandService.java
@@ -1,7 +1,4 @@
 package com.retailflow.productservice.brand.service;
 
-/**
- * Service interface for managing brand-related operations.
- */
 public interface BrandService {
 }
--- services/product-service/src/main/java/com/retailflow/productservice/brand/service/imp/BrandService/javaImp.java
+++ services/product-service/src/main/java/com/retailflow/productservice/brand/service/imp/BrandService/javaImp.java
@@ -1,8 +1,4 @@
 package com.retailflow.productservice.brand.service.imp.BrandService;
 
-/**
- * Implementation of the BrandService interface.
- */
 public class javaImp {
-
 }
